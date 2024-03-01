7f803b2d741540ac95c1f20005e7e849e198655c
Sekou CISSE <s.cisse78@gmail.com>
2024-03-01 17:37:50 +0100

    authenfication

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -10,11 +10,13 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 // use Symfony\Component\HttpFoundation\Request;
 // use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 use Symfony\Component\Serializer\SerializerInterface;
 
 #[Route('/api', name: 'app_api_')]
 class SecurityController extends AbstractController
 {
+    //VIDEO
     // public function __construct(
     //     private EntityManagerInterface $manager,
     //     private SerializerInterface $serializer
@@ -52,6 +54,26 @@ class SecurityController extends AbstractController
     //     );
     // }
 
+    // #[Route('/login', name: 'login', methods: 'POST')]
+    // public function login(#[CurrentUser] ?User $user): JsonResponse
+    // {
+    //     if (null === $user) {
+    //         return new JsonResponse(
+    //             ['message' => 'missing credentials'],
+    //             //status:
+    //             Response::HTTP_UNAUTHORIZED
+    //         );
+    //     }
+
+    //     return new JsonResponse(
+    //         [
+    //             'user' => $user->getUserIdentifier(),
+    //             'apiToken' => $user->getApiToken(),
+    //             'roles' => $user->getRoles()
+    //         ]
+    //     );
+    // }
+
     //COURS:
     public function __construct(
         private EntityManagerInterface $manager,
@@ -86,5 +108,21 @@ class SecurityController extends AbstractController
             ],
             Response::HTTP_CREATED
         );
+    }
+
+    #[Route('/login', name: 'login', methods: 'POST')]
+    public function login(#[CurrentUser] ?User $user): JsonResponse
+    {
+        if (null === $user) {
+            return new JsonResponse(
+                ['message' => 'Missing credentials'],
+                Response::HTTP_UNAUTHORIZED
+            );
+        }
+        return new JsonResponse([
+            'user'  => $user->getUserIdentifier(),
+            'apiToken' => $user->getApiToken(),
+            'roles' => $user->getRoles(),
+        ]);
     }
 }
